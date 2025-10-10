1c5bd35aa060144ac2c56c74ea0dcf878b5600d3
Lautaro Facundo Sosa <facundososa550@gmail.com>
2025-10-10 19:55:07 -0300

    Upgrade
    Actualizacion para compatibilidad con GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Web_Votacion/", // 👈 Esta línea es la clave
   plugins: [react()],
 })
