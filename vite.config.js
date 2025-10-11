78c3f9eab60cb3b2ee383f99427201aaff2c4ed5
Lautaro Facundo Sosa <facundososa550@gmail.com>
2025-10-11 20:43:28 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Web_Votacion/", // 👈 Esta línea es la clave
+  base: './', 
   plugins: [react()],
 })
